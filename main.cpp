34185980ee4d739f5dd3d8678627b2b711cdb089
BaranovAndrew413 <andrey-baranov1@mail.ru>
2020-04-28 13:53:18 +0400

    f

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <conio.h>
+#include <iostream>
+#include <string>
+#include<fstream>
+
+using namespace std;
+#include "TTextLink.h"
+#include "TText.h"
+
+
+TTextMem TTextLink::MemHeader;
+
+	int main() {
+		TTextLink::InitMemSystem(500);
+		TText t;
+		string ddd;
+		t.Read("text.txt");
+	}
+				
+			
+
+
+		
+
